be4365b795e0e21863066a79d35af72b2e08c54c
Wu-Chiang Chang <33931350+likemoonlight@users.noreply.github.com>
2022-07-24 21:45:31 +0800

    Add files via upload

---
+++ ForHalcon/ForHalcon/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using HalconDotNet;
+using ForHalcon;
+
+namespace ForHalcon
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ForHalcon.Program.hv_Path= "C://Users//Jason//Desktop//ForHalcon";
+           
+            string Path = "C://Users//Jason//Desktop//ForHalcon//1.jpg";
+            Bitmap imge = new Bitmap(@Path);
+            pictureBox1.Image = imge;
+            Thread.Sleep(30);
+            new HDevelopExport();
+            ShowData.Items.Clear();
+            ShowData.Items.Add("Class2_Number:");
+            ShowData.Items.Add(ForHalcon.Program.hv_Class2_Number);
+            ShowData.Items.Add("Class2_Ratio:");
+            ShowData.Items.Add(ForHalcon.Program.hv_Class2_Ratio);
+            ShowData.Items.Add("Class2_area:");
+            ShowData.Items.Add( ForHalcon.Program.hv_Class2_area);
+            ShowData.Items.Add("Class2_centerX:");
+            ShowData.Items.Add(ForHalcon.Program.hv_Class2_centerX);
+            ShowData.Items.Add("Class2_centerY:");
+            ShowData.Items.Add( ForHalcon.Program.hv_Class2_centerY);
+            ShowData.Items.Add("Class2_height:");
+            ShowData.Items.Add(ForHalcon.Program.hv_Class2_height);
+            ShowData.Items.Add("Class2_width:");
+            ShowData.Items.Add( ForHalcon.Program.hv_Class2_width);
+
+            HImage img = new HImage(@Path);
+            HRegion region = img.Threshold(0d, 122d);
+            int numRegions = region.Connection().CountObj();
+            Console.WriteLine("Number of Regions: " + numRegions);
+
+            HObject obj = new HObject();
+            HOperatorSet.ReadImage(out obj, Path);
+        }
+    }
+}
---
+++ ForHalcon/ForHalcon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace ForHalcon
+{
+    internal static class Program
+    {
+        public static HTuple hv_Path = null;
+        public static HTuple hv_Class2_Number = null;     //瑕疵數量
+        public static HTuple hv_Class2_Ratio = null; //寬高比
+        public static HTuple hv_Class2_area = null; //面積
+        public static HTuple hv_Class2_centerX = null;
+        public static HTuple hv_Class2_centerY = null; //中心座標  
+        public static HTuple hv_Class2_height = null;  //高
+        public static HTuple hv_Class2_width = null;  //寬
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+    }
+    }
+}
